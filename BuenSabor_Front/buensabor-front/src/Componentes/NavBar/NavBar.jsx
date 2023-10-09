f104cc7ba6cf70f5b0d150a0e8ffbbb151686258
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-10-09 20:17:44 -0300

    LinkTo/NavBar

--- BuenSabor_Front/buensabor-front/src/Componentes/NavBar/NavBar.jsx
+++ BuenSabor_Front/buensabor-front/src/Componentes/NavBar/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "../../Images/Buen sabor logo 1.png";
 import LoginAndCart from "../LoginAndCart/LoginAndCart";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg border border-dark border-2">
       <div className="container d-flex align-items-center">
-        <img src={logo} alt="Cargando.." />
+        <Link to={"/home"}>
+          <img src={logo} alt="Cargando.." />
+        </Link>
         <ul className="navbar-nav">
           <li className="nav-item d-flex align-items-center"></li>
           <li className="nav-item px-5">
