1f5dc4c29a80cde60204084aa71d6eccfb1e87ec
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-10-11 10:53:39 -0300

    formLogin/2

--- BuenSabor_Front/buensabor-front/src/Componentes/FormLogin/FormLogin.jsx
+++ BuenSabor_Front/buensabor-front/src/Componentes/FormLogin/FormLogin.jsx
@@ -1,20 +1,20 @@
 import React from "react";
+import image from "../../Images/imageForm.jpg";
 import "./FormLogin.css";
 
 export default function FormLogin() {
   return (
-    <section class="h-100 gradient-form">
-      <div className="container py-5 h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
+    <section className="container pt-5">
+      <div>
+        <div className="row d-flex justify-content-center align-items-center">
           <div className="col-xl-10">
-            <div className="card rounded-3 text-black">
-              <div className="row g-0">
+            <div>
+              <div className="row d-flex justify-content-center align-items-center">
                 <div className="col-lg-6">
                   <div className="card-body p-md-5 mx-md-4">
                     <div className="text-center">
                       <h1 className="mt-1 pb-1 fw-bold">Inicia Sesión</h1>
                     </div>
-
                     <form>
                       <div className="form-outline mb-4">
                         <input
@@ -41,11 +41,8 @@ export default function FormLogin() {
                           ¿Olvidaste tu contraseña?
                         </a>
                       </div>
-                      <div className="text-center">
-                        <button
-                          className="btn buttonLoggin btn-block fa-lg gradient-custom-2 mb-3"
-                          type="button"
-                        >
+                      <div className="text-center d-grid gap-2 col-12 mx-auto">
+                        <button className="btn buttonLoggin mb-3" type="button">
                           Aceptar
                         </button>
                       </div>
@@ -59,16 +56,13 @@ export default function FormLogin() {
                     </form>
                   </div>
                 </div>
-                <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
-                  <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                    <h4 className="mb-4">We are more than just a company</h4>
-                    <p className="small mb-0">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
+                <div className="col-lg-6 ">
+                  <img
+                    clasName="img-fluid"
+                    src={image}
+                    alt="Cargando.."
+                    style={{ width: "100%", height: "700px" }}
+                  />
                 </div>
               </div>
             </div>
