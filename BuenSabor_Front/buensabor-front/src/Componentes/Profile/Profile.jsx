9082c6d43ac0beb080fe42b5aa4b242d63f30197
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-10-14 23:51:01 -0300

    profile/icon/dropdown

--- BuenSabor_Front/buensabor-front/src/Componentes/Profile/Profile.jsx
+++ BuenSabor_Front/buensabor-front/src/Componentes/Profile/Profile.jsx
@@ -2,5 +2,38 @@ import React from "react";
 import "./Profile.css";
 
 export default function Profile() {
-  return <i className="bi bi-person-circle heightUserLogoProfile"></i>;
+  return (
+    <div className="dropdown">
+      <a
+        className="dropdown-toggle"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <i className="bi bi-person-circle heightUserLogoProfile"></i>
+      </a>
+
+      <ul className="dropdown-menu">
+        <li>
+          <a className="dropdown-item" href="/home/profile/updateData">
+            Editar Perfil
+          </a>
+        </li>
+        <li>
+          <a className="dropdown-item" href="#">
+            Mis Pedidos
+          </a>
+        </li>
+        <li>
+          <a className="dropdown-item" href="#">
+            Mi Carrito
+          </a>
+        </li>
+        <li>
+          <a className="dropdown-item" href="#">
+            Cerrar Sesion
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
