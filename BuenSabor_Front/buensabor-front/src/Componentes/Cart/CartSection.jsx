2c02a60272b7c7befdb3b25cf5a10a5fa84d307d
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-10-18 11:03:44 -0300

    CartSection/add/delete/substract

--- BuenSabor_Front/buensabor-front/src/Componentes/Cart/CartSection.jsx
+++ BuenSabor_Front/buensabor-front/src/Componentes/Cart/CartSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import food from "../../Data/comidas.json";
@@ -6,21 +6,80 @@ import food from "../../Data/comidas.json";
 export default function CartSection() {
   const foodInTheCart = JSON.parse(localStorage.getItem("foodInTheCart")) || [];
 
+  const [deleteProductCart, setDeleteProductCart] = useState(foodInTheCart);
+
   const foodCart = food.comidas.filter((comida) =>
-    foodInTheCart.includes(comida.id)
+    deleteProductCart.includes(comida.id)
   );
 
-  const uniqueFood = [];
+  const [productCounts, setProductCounts] = useState({}); // Cambiar el estado inicial a un objeto
+  const [addPrice, setAddPrice] = useState({}); // Cambiar el estado inicial a un objeto
 
-  if (foodCart && foodCart.length) {
-    for (let i = 0; i < foodCart.length; i++) {
-      const food = foodCart[i];
-      if (!uniqueFood.find((item) => item.id === food.id)) {
-        uniqueFood.push(food);
-      }
-    }
+  function handleAddIcon(foodId) {
+    // Clonar el objeto de recuento actual
+    const newCounts = { ...productCounts };
+    // Obtener el recuento actual del producto
+    const currentCount = newCounts[foodId] || 1;
+    // Incrementar el recuento del producto seleccionado
+    newCounts[foodId] = currentCount + 1;
+    // Actualizar el estado con el nuevo objeto de recuento
+    setProductCounts(newCounts);
+    // Clonar el objeto de precios adicionales actual
+    const newPrices = { ...addPrice };
+    // Obtener el precio del producto
+    const productPrice =
+      foodCart.find((food) => food.id === foodId)?.precio || 0;
+    // Calcular el precio total para el producto
+    const totalPrice = productPrice * (currentCount + 1);
+    // Actualizar el estado con el nuevo precio
+    newPrices[foodId] = totalPrice;
+    setAddPrice(newPrices);
   }
 
+  function handleSubtractIcon(foodId) {
+    // Clonar el objeto de recuento actual
+    const newCounts = { ...productCounts };
+    // Obtener el recuento actual del producto
+    const currentCount = newCounts[foodId] || 1;
+    // Incrementar el recuento del producto seleccionado
+    newCounts[foodId] = currentCount - 1;
+    // Actualizar el estado con el nuevo objeto de recuento
+    setProductCounts(newCounts);
+    // Clonar el objeto de precios adicionales actual
+    const newPrices = { ...addPrice };
+    // Obtener el precio del producto
+    const productPrice =
+      foodCart.find((food) => food.id === foodId)?.precio || 0;
+    // Calcular el precio total para el producto
+    const totalPrice = productPrice * (currentCount - 1);
+    // Actualizar el estado con el nuevo precio
+    newPrices[foodId] = totalPrice;
+    setAddPrice(newPrices);
+  }
+
+  function handleDeleteProduct(foodId) {
+    // Clonar el estado de productos eliminados actual
+    const updatedDeleteProductCart = [...deleteProductCart];
+    // Encontrar y eliminar el producto del array
+    const index = updatedDeleteProductCart.indexOf(foodId);
+    if (index > -1) {
+      updatedDeleteProductCart.splice(index, 1);
+    }
+    // Actualizar el estado con el nuevo array
+    setDeleteProductCart(updatedDeleteProductCart);
+
+    // Actualizar el localStorage sin el producto eliminado
+    const updatedLocalStorage =
+      JSON.parse(localStorage.getItem("foodInTheCart")) || [];
+    const productIndex = updatedLocalStorage.indexOf(foodId);
+    if (productIndex > -1) {
+      updatedLocalStorage.splice(productIndex, 1);
+      localStorage.setItem(
+        "foodInTheCart",
+        JSON.stringify(updatedLocalStorage)
+      );
+    }
+  }
   return (
     <section className="container">
       <div>
@@ -30,9 +89,9 @@ export default function CartSection() {
         <h1 className="fw-bold">Mi carrito</h1>
       </div>
       <div className="row d-flex justify-content-center">
-        <div className="col-10 ">
-          {uniqueFood.map((food) => (
-            <div className="row">
+        <div className="col-10">
+          {foodCart.map((food) => (
+            <div className="row" key={food.id}>
               <div className="d-flex align-items-center pb-5">
                 <div className="col-8">
                   <div className="d-flex align-items-center">
@@ -49,13 +108,30 @@ export default function CartSection() {
                 </div>
                 <div className="col-2">
                   <div className="d-flex align-items-center justify-content-center">
-                    <i class="bi bi-dash-circle heightCart"></i>
-                    <h4 className="text-center px-2">1</h4>
-                    <i class="bi bi-plus-circle heightCart"></i>
+                    {productCounts[food.id] <= 1 || !productCounts[food.id] ? (
+                      <i
+                        className="bi bi-x-circle heightCart"
+                        onClick={() => handleDeleteProduct(food.id)}
+                      ></i>
+                    ) : (
+                      <i
+                        className="bi bi-dash-circle heightCart"
+                        onClick={() => handleSubtractIcon(food.id)}
+                      ></i>
+                    )}
+                    <h4 className="text-center px-2">
+                      {productCounts[food.id] || 1}
+                    </h4>
+                    <i
+                      className="bi bi-plus-circle heightCart"
+                      onClick={() => handleAddIcon(food.id)}
+                    ></i>
                   </div>
                 </div>
                 <div className="col-2">
-                  <h5 className="text-center">$ {food.precio}</h5>
+                  <h5 className="text-center">
+                    $ {addPrice[food.id] || food.precio}
+                  </h5>
                 </div>
               </div>
             </div>
